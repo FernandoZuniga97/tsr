89527d4f864ccfb64d2ae9a60a0833cfa77d08d6
FernandoZuniga97 <fzunigao@unah.hn>
2024-03-15 22:20:03 -0600

    Se añadio en start chats base sin personalizacion

--- hakuna_matata/lib/pages/start.dart
+++ hakuna_matata/lib/pages/start.dart
@@ -4,18 +4,83 @@ import 'package:flutter/material.dart';
 class Start extends StatelessWidget {
   const Start({super.key,});
 
+static const List<Map<String, dynamic>> chats = [
+    {
+      "nombre": "Popeye el pollo chuco",
+      "mensaje": "u wan a querer chilation?",
+      "hora": "23:00",
+      "foto":
+          FlutterLogo(),
+    },
+    {
+      "nombre": "Pedro Lopez",
+      "mensaje": "Vamno a juga",
+      "hora": "15:00",
+      "foto": FlutterLogo(),
+    },
+    {
+      "nombre": "Luis Perez",
+      "mensaje": "volvio tu ex",
+      "hora": "09:00",
+      "foto": FlutterLogo(),
+    },
+    {
+      "nombre": "Oscar Sanchez",
+      "mensaje": "SOAB",
+      "hora": "14:30",
+      "foto": FlutterLogo(),
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Inicio'),
+    return ListView.builder(
+      itemCount: chats.length,
+      itemBuilder: (context, index) {
+        if (chats.isEmpty) {
+          return const Center(
+            child: Text('No hay chats'),
+          );
+        }
+
+        return ItemChat(
+          nombre: chats[index]['nombre']!,
+          mensaje: chats[index]['mensaje']!,
+          hora: chats[index]['hora']!,
+          foto: chats[index]['foto'],
+        );
+      },
+    );
+  }
+}
+
+class ItemChat extends StatelessWidget {
+  const ItemChat({
+    super.key,
+    required this.nombre,
+    required this.mensaje,
+    required this.hora,
+    required this.foto,
+  });
+
+  final String nombre;
+  final String mensaje;
+  final String hora;
+  final Widget foto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Card(
+        child: ListTile(
+          title: Text(nombre),
+          subtitle: Text(mensaje),
+          leading: foto,
+          trailing: Text(hora),
         ),
       ),
     );
   }
 }
+

57866c44402e88fefa73fcbc44b88fd55a3fdb2a
FernandoZuniga97 <fzunigao@unah.hn>
2024-03-16 17:17:07 -0600

    Se empezo a personalizar el home

--- hakuna_matata/lib/pages/home.dart
+++ hakuna_matata/lib/pages/home.dart
@@ -10,53 +10,123 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Home pages'),
+        title: const Center( child: Text('Login')),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          
-          children: [
-            TextField(
-                  controller: emailController,
-                  maxLength: 30,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                    label: Text('Email'),
-                    icon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: -40,
+                    height: 400,
+                    width: width,
+                    child:Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/background.png'),
+                          fit: BoxFit.fill
+                        )
+                      ),
+                    ),
                   ),
-                ),
-                //custom pa que sea privado
-                Custompass(
-                  controller: passwordController,
-                  obscureText: true,
-                ),
-            PrimaryButton(
-              text: 'iniciar sesion',
-              onPressed: () {
-                if (emailController.text.isEmpty || passwordController.text.isEmpty ||  emailController.text != 'fzunigao@unah.hn' || passwordController.text != '123' ) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('El correo o/y la contraseña son incorrectos.')));
-                  return; //const SnackBar(content: Text('Los campos no pueden estar vacios'));
-                }else if( emailController.text == 'fzunigao@unah.hn' && passwordController.text == '123'){
-                Navigator.pushNamed(context, MyRoutes.start.name);
-                }
-                
-                
-              },
+                  Positioned(
+                    height: 400,
+                    width: width+20,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/background-2.png'),
+                          fit: BoxFit.fill
+                        )
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    height: 400,
+                    width: width+20,
+                    child:Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/background-2.png'),
+                          fit: BoxFit.fill
+                        )
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-            SecondaryButton(
-              text: ' Registro',
-              onPressed: () {
-                // * Navegación a otra pantalla y reemplazar
-                //* la pila de navegación
-                Navigator.pushNamed(context, MyRoutes.registro.name);
-              },
-            ),
-          ],
+                Container(
+                  padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                            border: Border(bottom: BorderSide(
+                              color: Color.fromRGBO(196, 135, 198, .3)
+                            ))
+                          ),
+                  child: TextField(
+                        controller: emailController,
+                        maxLength: 30,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          label: Text('Email'),
+                          labelStyle: TextStyle(color: Colors.purple),
+                          icon: Icon(Icons.email),
+                          border: UnderlineInputBorder(),
+                          
+                        ),
+                      ),
+          ),
+                      //custom pa que sea privado
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          border: Border(bottom: BorderSide(
+                            color: Color.fromRGBO(196, 135, 198, .3)
+                          ))
+                        ),
+                      child: Custompass(
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      ),
+                  PrimaryButton(
+                    text: 'iniciar sesion',
+                    onPressed: () {
+                      if (emailController.text.isEmpty || passwordController.text.isEmpty ||  emailController.text != 'fzunigao@unah.hn' || passwordController.text != '123' ) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('El correo o/y la contraseña son incorrectos.')));
+                        return; //const SnackBar(content: Text('Los campos no pueden estar vacios'));
+                      }else if( emailController.text == 'fzunigao@unah.hn' && passwordController.text == '123'){
+                      Navigator.pushNamed(context, MyRoutes.start.name);
+                      }
+                      
+                      
+                    },
+                  ),
+                  SecondaryButton(
+                    text: ' Registro',
+                    onPressed: () {
+                      // * Navegación a otra pantalla y reemplazar
+                      //* la pila de navegación
+                      Navigator.pushNamed(context, MyRoutes.registro.name);
+                    },
+                  ),       
+            ]
+               // 
+              
+                  
+            //
+          ),
         ),
       ),
     );
@@ -73,9 +143,10 @@ class PrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return ElevatedButton( 
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue, // color de fondo
+        shape: const StadiumBorder(),
+        backgroundColor:  const Color.fromRGBO(49, 39, 79, 1), // color de fondo
         foregroundColor: Colors.white, // color de texto
       ),
       onPressed: onPressed,
@@ -123,6 +194,7 @@ class _CustomInputState extends State<Custompass> {
       obscureText: obscureText,
       decoration: InputDecoration(
         label: const Text('Password'),
+        labelStyle: const TextStyle(color: Colors.purple),
         prefixIcon: const Icon(Icons.lock),
         suffixIcon: IconButton(
             onPressed: () {
@@ -131,7 +203,7 @@ class _CustomInputState extends State<Custompass> {
               }); 
             },
             icon: Icon( obscureText ? Icons.visibility : Icons.visibility_off)),
-        border: const OutlineInputBorder(),
+        border: const UnderlineInputBorder(),
       ),
     );
   }

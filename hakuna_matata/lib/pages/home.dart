21e88fc7a3496cd21ff4aa695bb21b09ffd2d73f
FernandoZuniga97 <fzunigao@unah.hn>
2024-03-16 00:35:12 -0600

    se añadio la contraseña con el obscuretext y se puso en privado por si las moscas

--- hakuna_matata/lib/pages/home.dart
+++ hakuna_matata/lib/pages/home.dart
@@ -2,7 +2,13 @@ import 'package:hakuna_matata/routes/myroutes.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key,});
+  Home({super.key,});
+  
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nombreController = TextEditingController();
+  final apellidoController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +19,23 @@ class Home extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
+          
           children: [
+            TextField(
+                  controller: emailController,
+                  maxLength: 30,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    label: Text('Email'),
+                    icon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                //custom pa que sea privado
+                Custompass(
+                  controller: passwordController,
+                  obscureText: true,
+                ),
             PrimaryButton(
               text: 'iniciar sesion',
               onPressed: () {
@@ -68,6 +90,44 @@ class SecondaryButton extends StatelessWidget {
     return TextButton(
       onPressed: onPressed,
       child: Text(text),
+    );
+  }
+}
+
+class Custompass extends StatefulWidget {
+  Custompass({super.key,
+  required this.controller,
+    this.obscureText = false,
+  });
+  final TextEditingController controller;
+  bool obscureText;
+
+  @override
+  State<Custompass> createState() => _CustomInputState();
+}
+
+class _CustomInputState extends State<Custompass> {
+  bool obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+  
+    return TextField(
+      
+      controller: widget.controller,
+      keyboardType: TextInputType.text,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        label: const Text('Password'),
+        prefixIcon: const Icon(Icons.lock),
+        suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                obscureText = !obscureText;
+              }); 
+            },
+            icon: Icon( obscureText ? Icons.visibility : Icons.visibility_off)),
+        border: const OutlineInputBorder(),
+      ),
     );
   }
 }
--- hakuna_matata/lib/routes/router.dart
+++ hakuna_matata/lib/routes/router.dart
@@ -6,7 +6,7 @@ import 'package:hakuna_matata/pages/start.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
-  MyRoutes.home.name: (context) => const Home(),
+  MyRoutes.home.name: (context) =>  Home(),
   MyRoutes.registro.name: (context) => const Registro(),
   MyRoutes.start.name: (context) => const Start(),
 };
